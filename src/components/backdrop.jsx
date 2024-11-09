c7ea26ec960b170adc76f06683e52ac42ec3eba7
Devanoyudhistira <devanotira@gmail.com>
2024-11-09 09:30:13 +0700

    feat:add backdrop in navbar so it not gonna intervents other element when clicked

---
+++ src/components/backdrop.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable react/prop-types */
+import { motion } from "framer-motion"
+export default function Backdrop({children,onclose}){
+    return (<motion.div initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} exit={{ opacity: 0, scale: 0 }} onClick={onclose} className="bg-black/40 w-screen h-screen absolute z-50 top-0 left-0" >
+        {children}
+    </motion.div>)
+}
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,7 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 import { Github, Instagram } from "react-bootstrap-icons";
+import Backdrop from "./backdrop";
 
 /* eslint-disable react/prop-types */
 export default function Navbar({ logout, name, image }) {
@@ -8,14 +9,17 @@ export default function Navbar({ logout, name, image }) {
   return (
     <nav className=" w-screen box-border h-auto relative flex items-center justify-between px-2 py-1">
       <AnimatePresence initial={false} mode="wait" onExitComplete={() => null}>
-        {isshownav && <motion.div initial={{ opacity:0,scaleY:0 }} animate={{ opacity:1,scaleY:1 }} exit={{ opacity:0,scaleY:0 }} className="absolute flex flex-col gap-2 right-4 top-8 w-max h-max px-4 py-2 bg-white shadow-[2px_2px_8px_black] z-50" >
-          <h1>{name}</h1>
-          <ul>
-            <li className="listicon" > <Instagram className="inline-block mr-2 " /> <a href="https://www.instagram.com/devano15.08/?next=%2F">instagram</a></li>
-            <li className="listicon" > <Github className="inline-block mr-2 text-black" /> <a href="https://github.com/Devanoyudhistira"> github</a></li>
-          </ul>
-          <button onClick={logout} className=" font-inter text-xl px-2 py-1 rounded-xl bg-red-600">logout</button>
-        </motion.div>}
+        {isshownav &&
+          <Backdrop onclose={() => setishownav(!isshownav)} >
+             <motion.div className="absolute flex flex-col gap-2 right-4 top-8 w-max h-max px-4 py-2 bg-white shadow-[2px_2px_8px_black] z-50" >
+              <h1>{name}</h1>
+              <ul>
+                <li className="listicon" > <Instagram className="inline-block mr-2 " /> <a href="https://www.instagram.com/devano15.08/?next=%2F">instagram</a></li>
+                <li className="listicon" > <Github className="inline-block mr-2 text-black" /> <a href="https://github.com/Devanoyudhistira"> github</a></li>
+              </ul>
+              <button onClick={logout} className=" font-inter text-xl px-2 py-1 rounded-xl bg-red-600">logout</button>
+            </motion.div>
+          </Backdrop>}
       </AnimatePresence>
       <h1 className="font-bebas ml-1 text-2xl tracking-wider" >good morning {name} </h1>
       <img
